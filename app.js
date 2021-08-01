2f442e7a19b07fe50f2544184c0e4c5e4d0aebbc
eren.yener <eren.yener@trendyol.com>
2021-08-01 13:10:04 +0300

    change port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
